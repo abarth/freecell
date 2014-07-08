b0002462fb04bbd912302f0914f03b23e4aa7501
Adam Barth <abarth@clearzen>
2014-07-08 12:35:15 -0700

    Add towers

--- web/tableau/tableau.dart
+++ web/tableau/tableau.dart
@@ -9,19 +9,24 @@ import '../deck/deck.dart';
 part 'pile.dart';
 part 'column.dart';
 part 'cell.dart';
+part 'tower.dart';
 
 const int kNumberOfColumns = 8;
 const int kNumberOfCells = 4;
+const int kNumberOfTowers = 4;
 
 class Tableau {
   List<Column> columns;
   List<Cell> cells;
+  List<Tower> towers;
 
   Tableau() {
     columns = new List<Column>.from(
         new Iterable.generate(kNumberOfColumns, (i) => new Column()));
     cells = new List<Cell>.from(
         new Iterable.generate(kNumberOfCells, (i) => new Cell()));
+    towers = new List<Tower>.from(
+        new Iterable.generate(kNumberOfTowers, (i) => new Tower()));
   }
 
   void deal(Deck deck) {
---
+++ web/tableau/tower.dart
@@ -0,0 +1,17 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+part of freecell.tableau;
+
+class Tower extends Pile {
+  bool _canAccept(Card card) {
+    if (cards.isEmpty)
+      return card.rank.value == 1;
+    Card current = cards.last;
+    return current.rank.value + 1 == card.rank.value
+        && current.suit == card.suit;
+  }
+
+  bool _canTake() => false;
+}
