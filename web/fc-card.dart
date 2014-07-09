a9e351ca7981daec06d0108ec2b90c29e8994e42
Elliott Sprehn <esprehn@chromium.org>
2014-07-08 18:12:40 -0700

    Fix dragging to remove the cards from the stack.

--- web/fc-card.dart
+++ web/fc-card.dart
@@ -1,6 +1,4 @@
 import "package:polymer/polymer.dart";
-import 'dart:html';
-import 'dart:js';
 
 import "deck/deck.dart";
 
@@ -11,38 +9,9 @@ class FcCard extends PolymerElement {
   FcCard.created() : super.created() {
   }
 
-  String imageUrl(Card card) {
+  String get url => formatUrl(card);
+
+  String formatUrl(Card card) {
     return "svg-cards/${ card.rank.value }_of_${ card.suit.name }.svg";
-  }
-
-  void handleDragStart(Event event, JsObject dragInfo, Node target) {
-    FcCard target = event.target;
-    Element avatar = dragInfo["avatar"];
-
-    // FIXME: The card has a funny clipping rect when dragging.
-    avatar.style.setProperty("will-change", "transform");
-
-    ImageElement img = document.createElement("img");
-    img.src = imageUrl(target.card);
-
-    Rectangle rect = target.getBoundingClientRect();
-    img.style.width = "${rect.width}px";
-    img.style.height = "${rect.height}px";
-    img.style.transform = "translate(-50%, -50%)";
-
-    avatar.append(img);
-
-    dragInfo["drag"] = (var dragInfo) {
-
-    };
-
-    dragInfo["drop"] = (var dragInfo) {
-      // FIXME: Polymer adds this expando, but dart won't let me get it
-      // since it's an expando.
-      var event = new JsObject.fromBrowserObject(dragInfo["event"]);
-      Element relatedTarget = event["relatedTarget"];
-      relatedTarget.dispatchEvent(new CustomEvent("drop-card", detail:this.card));
-      img.remove();
-    };
   }
 }
--- web/fc-pile.dart
+++ web/fc-pile.dart
@@ -1,7 +1,10 @@
 import "package:polymer/polymer.dart";
-import 'dart:html';
+import "dart:html";
+import "dart:js";
 
 import "tableau/tableau.dart";
+
+import "fc-card.dart";
 
 @CustomTag("fc-pile")
 class FcPile extends PolymerElement {
@@ -11,10 +14,48 @@ class FcPile extends PolymerElement {
     // FIXME: on-* annotations listen on the ShadowRoot,
     // not the host, so we need to do it here for the
     // super class.
-    on["drop-card"].listen(handleDropCard);
+  }
+
+  void attached() {
+    this.shadowRoots.forEach((String className, ShadowRoot root) {
+      root.on["drop-card"].listen(handleDropCard);
+      root.on["drag-start"].listen(handleDragStart);
+    });
   }
 
   void handleDropCard(CustomEvent event) {
-    pile.accept(event.detail);
+    if (!pile.accept(event.detail))
+      event.preventDefault();
+  }
+
+  void handleDragStart(CustomEvent event) {
+    JsObject dragInfo = new JsObject.fromBrowserObject(event)["detail"];
+    FcCard target = event.target;
+    Element avatar = dragInfo["avatar"];
+
+    // FIXME: The card has a funny clipping rect when dragging.
+    avatar.style.setProperty("will-change", "transform");
+
+    ImageElement img = document.createElement("img");
+    img.src = target.url;
+
+    Rectangle rect = target.getBoundingClientRect();
+    img.style.width = "${rect.width}px";
+    img.style.height = "${rect.height}px";
+    img.style.transform = "translate(-50%, -50%)";
+
+    avatar.append(img);
+
+    dragInfo["drag"] = (var dragInfo) { };
+
+    dragInfo["drop"] = (var dragInfo) {
+      // FIXME: Polymer adds this expando, but dart won't let me get it
+      // since it's an expando.
+      var event = new JsObject.fromBrowserObject(dragInfo["event"]);
+      Element relatedTarget = event["relatedTarget"];
+      if (relatedTarget.dispatchEvent(new CustomEvent("drop-card", detail:target.card)))
+        pile.cards.remove(target.card);
+      img.remove();
+    };
   }
 }
