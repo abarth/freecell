a0aba5614016f58f7b6b7c6c5edab911112c45d4
Elliott Sprehn <esprehn@chromium.org>
2014-07-09 12:03:14 -0700

    Dispatch events from <fc-drag-drop>.

--- web/fc-drag-drop.dart
+++ web/fc-drag-drop.dart
@@ -1,10 +1,26 @@
 import "package:polymer/polymer.dart";
 import "dart:html";
+import "dart:js";
+import "dart:math";
 import "dart:async";
+
+class FcDragDetail {
+  Point location;
+
+  FcDragDetail(this.location);
+}
+
+class FcDropDetail {
+  Point location;
+  Element zone;
+
+  FcDropDetail(this.location, this.zone);
+}
 
 @CustomTag("fc-drag-drop")
 class FcDragDrop extends PolymerElement {
-  bool dragging = false;
+  @observable bool dragging = false;
+
   StreamSubscription trackStartListener;
   StreamSubscription trackListener;
   StreamSubscription trackEndListener;
@@ -25,20 +41,28 @@ class FcDragDrop extends PolymerElement {
   }
 
   void handleTrackStart(CustomEvent event) {
-    this.fire("drag-start");
     this.dragging = true;
   }
 
   void handleTrack(CustomEvent event) {
     if (!this.dragging)
       return;
-    // FIXME: Compute the x, y here and call drag().
+    this.fire("fc-drag", detail:new FcDragDetail(_pointFromEvent(event)));
   }
 
   void handleTrackEnd(CustomEvent event) {
     if (!this.dragging)
       return;
-    this.dragging = false;
-    // FIXME: Call drop().
+    this.fire("fc-drop", detail:new FcDropDetail(_pointFromEvent(event), _relatedTargetFromEvent(event)));
+  }
+
+  Point _pointFromEvent(CustomEvent event) {
+    JsObject rawEvent = new JsObject.fromBrowserObject(event);
+    return new Point(rawEvent["clientX"], rawEvent["clientY"]);
+  }
+
+  Element _relatedTargetFromEvent(CustomEvent event) {
+    JsObject rawEvent = new JsObject.fromBrowserObject(event);
+    return rawEvent["relatedTarget"];
   }
 }
